df361af2efd038934ec953b2bc13fd78da5292b1
ImJustExisting <159423284+ImJustExisting@users.noreply.github.com>
2026-01-26 12:54:37 +0100

    added pages folder and some router aspects

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,15 +1,25 @@
-import Header from './components/header/header.tsx'
-import './App.css'
-import Footer from './components/footer/footer.tsx'
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import Header from "./components/header/header.tsx";
+import Footer from "./components/footer/footer.tsx";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
