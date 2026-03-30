0a4cd9c6ee842cf6e6741d720eae231bdc3c763c
ImJustExisting <159423284+ImJustExisting@users.noreply.github.com>
2026-03-30 04:18:31 +0200

    typo fix

--- portfolio/src/stories/Button.stories.tsx
+++ portfolio/src/stories/Button.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import Button from "./button.tsx";
+import Button from "./Button";
 
 const meta: Meta<typeof Button> = {
   title: "UI/Button",
